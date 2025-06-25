c0e83151c79fac41ddcffe8eb6e468b2a7333357
XBP <xavierbaubes@gmail.com>
2025-06-25 11:13:43 +0200

    icon on .exe windows, path works running python too

--- buscamines/main.py
+++ buscamines/main.py
@@ -1,20 +1,25 @@
-from tkinter import Tk, PhotoImage
+from tkinter import Tk
 import os
+import sys
 
 from config import CONFIGURACIO
 from buscamines import Buscamines
 
+def resource_path(rel_path): # Aconsegueix el path absolut tant en .py com en .exe
+    try:
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, rel_path)
+
 if __name__ == "__main__": # Executa el codi nomes si el fitxer s executa directament, no quan s importa
-    
     root = Tk() # Crea finestra principal de l aplicacio amb Tkinter
     root.title("Buscamines")
     root.resizable(False, False) # No permet que l'usuari redimensioni la finestra
 
-    ruta_arrel = os.path.dirname(__file__) # Ruta absoluta de la carpeta on es guarda aquest fitxer (main.py)
-    ruta_icona = os.path.join(ruta_arrel, "..", "icon", "bomba.png")
+    ruta_icona = resource_path("icon/bomba.ico")
     if os.path.exists(ruta_icona):
-        icona = PhotoImage(file=ruta_icona)
-        root.iconphoto(False, icona)
+        root.iconbitmap(ruta_icona)
 
     Buscamines(root, CONFIGURACIO)
     root.mainloop()
